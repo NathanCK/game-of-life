855b275b25f42975e21e263196cebebbb34e6137
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-04-06 20:31:09 -0400

    removed bloc code

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -26,30 +26,21 @@ class GameBoard extends StatefulWidget {
 
 class _GameBoardState extends State<GameBoard>
     with TickerProviderStateMixin<GameBoard> {
-  static const int _ALIVE = 1;
-  static const int _DEAD = 0;
-
   late Animation<Set<int>> animation;
   late AnimationController controller;
 
   late final Tween<Set<int>> _rotationTween;
 
   late final Set<int> aliveCells;
-  late final int totalCellCount;
   late final int rowCount;
   late final int colCount;
-  late final List<int> cellStatus;
 
   @override
   void initState() {
     super.initState();
-    totalCellCount = (widget.height * widget.width).toInt() ~/
-        (widget.cellSize * widget.cellSize);
 
     rowCount = widget.height ~/ widget.cellSize;
     colCount = widget.width ~/ widget.cellSize;
-
-    cellStatus = List.filled(rowCount * colCount, _DEAD);
 
     final start1Row =
         Random().nextInt(rowCount - 3); // make sure there is space.
@@ -84,7 +75,7 @@ class _GameBoardState extends State<GameBoard>
       cellStatus[aliveCell] = _ALIVE;
     }
 
-    _rotationTween = Tween(begin: {}, end: aliveCells);
+    _rotationTween = Tween(begin: {}, end: {});
 
     controller = AnimationController(
       vsync: this,
@@ -105,63 +96,6 @@ class _GameBoardState extends State<GameBoard>
         controller.forward();
       }
     });
-  }
-
-  void nextGeneration() {
-    final Set<int> deadCellCandidates = {};
-    final Set<int> aliveCellCandidates = {};
-    for (int r = 0; r < rowCount; r++) {
-      for (int c = 0; c < colCount; c++) {
-        final currentIndex = r * colCount + c;
-
-        final topIndex = r - 1 >= 0 ? (r - 1) * colCount + c : -1;
-        final topLeftIndex =
-            r - 1 >= 0 && c - 1 >= 0 ? (r - 1) * colCount + (c - 1) : -1;
-        final topRightIndex =
-            r - 1 >= 0 && c + 1 < colCount ? (r - 1) * colCount + (c + 1) : -1;
-        final leftIndex = c - 1 >= 0 ? r * colCount + (c - 1) : -1;
-        final rightIndex = c + 1 < colCount ? r * colCount + (c + 1) : -1;
-        final botIndex = r + 1 < rowCount ? (r + 1) * colCount + c : -1;
-        final botLeftIndex =
-            r + 1 < rowCount && c - 1 >= 0 ? (r + 1) * colCount + (c - 1) : -1;
-        final botRightIndex = r + 1 < rowCount && c + 1 < colCount
-            ? (r + 1) * colCount + (c + 1)
-            : -1;
-
-        final indexes = {
-          topIndex,
-          topLeftIndex,
-          topRightIndex,
-          leftIndex,
-          rightIndex,
-          botIndex,
-          botLeftIndex,
-          botRightIndex
-        }..remove(-1);
-
-        int aliveNeighbors = 0;
-
-        for (int neighborIndex in indexes) {
-          aliveNeighbors += cellStatus[neighborIndex];
-        }
-
-        if (aliveNeighbors < 2 || aliveNeighbors > 3) {
-          deadCellCandidates.add(currentIndex);
-        } else if (aliveNeighbors == 3 && !aliveCells.contains(currentIndex)) {
-          aliveCellCandidates.add(currentIndex);
-        }
-      }
-    }
-
-    for (final deadCellIndex in deadCellCandidates) {
-      cellStatus[deadCellIndex] = _DEAD;
-      aliveCells.remove(deadCellIndex);
-    }
-
-    for (final aliveCellIndex in aliveCellCandidates) {
-      cellStatus[aliveCellIndex] = _ALIVE;
-      aliveCells.add(aliveCellIndex);
-    }
   }
 
   @override
