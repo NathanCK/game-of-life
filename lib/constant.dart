6270c93aa09111c61b1640f9a8bfc67d5e586a5d
Nathan Chan <kinnathanchan@gmail.com>
2025-09-12 13:58:51 -0700

    constants for default settings and colors

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,25 @@
+import 'package:flutter/material.dart';
+
 class Constant {
   static const int alive = 1;
   static const int dead = 0;
+}
+
+class DefaultGameSettings {
+  static const double cellSize = 7.0;
+  static const bool showControlPanel = true;
+  static const bool autoStart = false;
+  static const Duration speed = Duration(milliseconds: 500);
+}
+
+class DefaultLightColors {
+  static const Color alive = Colors.black;
+  static final Color dead = Colors.grey[50]!;
+  static const Color gridLine = Colors.white;
+}
+
+class DefaultDarkColors {
+  static const Color alive = Colors.black;
+  static final Color dead = Colors.grey[850]!;
+  static const Color gridLine = Colors.white;
 }
