faf89dde75ac0417891063fb741dc4b2a7d3ad2d
Nathan Chan <kinnathanchan@gmail.com>
2025-09-12 15:17:36 -0700

    removed fixed height and width. Used AnimatedIcon for play/pause button

--- lib/game_controller_bar.dart
+++ lib/game_controller_bar.dart
@@ -1,55 +1,104 @@
-import 'package:conway_game_of_life/bloc/game_board_bloc.dart';
+import 'package:conway_game_of_life/cubit/game_control_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class GameControllerBar extends StatelessWidget {
-  final double hight = 40; // TODO: should resize based on screen size.
-  final double wight = 160;
-  final GameBoardBloc _gameBoardBloc;
-
-  const GameControllerBar({super.key, required GameBoardBloc gameBoardBloc})
-      : _gameBoardBloc = gameBoardBloc;
+  const GameControllerBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: hight,
-      width: wight,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 2,
+      ),
+      margin: const EdgeInsets.only(bottom: 10),
       decoration: BoxDecoration(
         color: const Color(0xFFB19E6D),
         borderRadius: BorderRadius.circular(10),
         border: Border.all(color: Colors.black),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          _ControlButton(
-            iconWidget: const Icon(
-              Icons.play_arrow,
-            ),
-            onPressed: () {
-              _gameBoardBloc.add(GameResumeRequested());
-            },
-          ),
-          _ControlButton(
-            iconWidget: const Icon(
-              Icons.pause,
-            ),
-            onPressed: () {
-              _gameBoardBloc.add(GamePauseRequested());
-            },
-          ),
-          _ControlButton(
-            iconWidget: SvgPicture.asset(
-              'assets/icons/reset.svg',
-            ),
-            onPressed: () {
-              _gameBoardBloc.add(GameResetRequested());
-            },
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 4.0,
+            children: [
+              _PlayButton(),
+              _ControlButton(
+                iconWidget: SvgPicture.asset(
+                  'assets/icons/reset.svg',
+                ),
+                onPressed: () {
+                  context.read<GameControlCubit>().resetGame();
+                },
+              ),
+            ],
           ),
         ],
       ),
     );
+  }
+}
+
+class _PlayButton extends StatefulWidget {
+  @override
+  State<_PlayButton> createState() => _PlayButtonState();
+}
+
+class _PlayButtonState extends State<_PlayButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    if (context.read<GameControlCubit>().state.running) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<GameControlCubit, GameControlState>(
+      listener: (context, state) {
+        if (state.running) {
+          _controller.forward();
+        } else {
+          _controller.reverse();
+        }
+      },
+      builder: (context, state) {
+        final onPressed = state.running
+            ? context.read<GameControlCubit>().stopGame
+            : context.read<GameControlCubit>().startGame;
+
+        return _ControlButton(
+          iconWidget: AnimatedIcon(
+            icon: AnimatedIcons.play_pause,
+            progress: _animation,
+          ),
+          onPressed: onPressed,
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
 
